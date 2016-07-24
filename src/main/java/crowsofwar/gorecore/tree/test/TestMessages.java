b4d39c641c4132c5622bf96b0170dcdb7def6ac8
CrowsOfWar <ofwarcrows@gmail.com>
2016-07-24 09:36:34 -0500

    Testing if MessageConfiguration works

--- src/main/java/crowsofwar/gorecore/tree/test/TestMessages.java
+++ src/main/java/crowsofwar/gorecore/tree/test/TestMessages.java
@@ -1,16 +1,19 @@
 package crowsofwar.gorecore.tree.test;
 
 import crowsofwar.gorecore.chat.ChatMessage;
+import crowsofwar.gorecore.chat.MessageConfiguration;
+import net.minecraft.util.EnumChatFormatting;
+
 import static crowsofwar.gorecore.chat.ChatSender.newChatMessage;
 
 public class TestMessages {
 	
-	// TODO Fix: Making a multimessage that uses messages from different ChatSenders doesn't display ALL messages.
-	public static final ChatMessage MSG_VIDEOGAME_HELP = newChatMessage("test.buyVideogames.help");
-	public static final ChatMessage MSG_CAKE_FROST_HELP = newChatMessage("test.frostCake.help");
-	public static final ChatMessage MSG_CAKE_LICK_HELP = newChatMessage("test.lickCake.help");
-	public static final ChatMessage MSG_PLAYVIDEOGAMES_HELP = newChatMessage("test.videogames.help");
-	public static final ChatMessage MSG_CHATSENDER_HELP = newChatMessage("test.chatSender.help");
-	public static final ChatMessage MSG_VIDEOGAME_BRANCH_HELP = newChatMessage("test.videogamesBranch.help");
+	public static final MessageConfiguration CFG = new MessageConfiguration().addColor("special", EnumChatFormatting.LIGHT_PURPLE);
+	public static final ChatMessage MSG_VIDEOGAME_HELP = newChatMessage(CFG, "test.buyVideogames.help");
+	public static final ChatMessage MSG_CAKE_FROST_HELP = newChatMessage(CFG, "test.frostCake.help");
+	public static final ChatMessage MSG_CAKE_LICK_HELP = newChatMessage(CFG, "test.lickCake.help");
+	public static final ChatMessage MSG_PLAYVIDEOGAMES_HELP = newChatMessage(CFG, "test.videogames.help");
+	public static final ChatMessage MSG_CHATSENDER_HELP = newChatMessage(CFG, "test.chatSender.help");
+	public static final ChatMessage MSG_VIDEOGAME_BRANCH_HELP = newChatMessage(CFG, "test.videogamesBranch.help");
 	
 }
